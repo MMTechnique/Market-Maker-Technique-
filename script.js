59f2682987b61119da6838fb9efaa6015eef5f7a
MMTechnique <joelliott.a.iglesias@gmail.com>
2025-01-04 05:03:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openPopup(paymentMethod) {
+    var email = prompt("Please enter your email address:");
+    if (email) {
+        var paymentAddress = document.querySelector('.payment-address');
+        var paymentProof = prompt("Please provide proof of payment:");
+        var message = "Email: " + email + "\nPayment Proof: " + paymentProof;
+        window.open("mailto:RichForever9@protonmail.com?subject=Market Makers Technique Purchase - " + paymentMethod + "&body=" + message);
+    }
+}
