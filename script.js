8bd3feae7d3138ff299d26e84f431ad4a4a6ef71
MMTechnique <joelliott.a.iglesias@gmail.com>
2025-01-06 04:44:01 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openPopup(paymentMethod) {
-    var email = prompt("Please enter your email address:");
-    if (email) {
-        var paymentProof = prompt("Please provide proof of payment:");
-        var message = "Email: " + email + "\nPayment Proof: " + paymentProof;
-        window.open("mailto:RichForever9@protonmail.com?subject=Market Makers Technique Purchase - " + paymentMethod + "&body=" + message);
-    }
-}
